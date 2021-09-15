5bf6fcb4b6753d39db73d59e4f75a51978aa7287
Jean-Louis Tournay <jlt@jlt.com>
2021-09-15 10:53:03 +0400

    scripts: code gen
    Adapt to go mod
    
    Fixes #1

--- hack/k8s/codegen/update-generated.sh
+++ hack/k8s/codegen/update-generated.sh
@@ -4,10 +4,12 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
-vendor/k8s.io/code-generator/generate-groups.sh \
+
+hack/k8s/codegen/generate-groups.sh \
   "all" \
   "github.com/coreos/etcd-operator/pkg/generated" \
   "github.com/coreos/etcd-operator/pkg/apis" \
   "etcd:v1beta2" \
   --go-header-file "./hack/k8s/codegen/boilerplate.go.txt" \
-  $@
+  --output-base "$(dirname "${BASH_SOURCE[0]}")/../../../pkg" \
+
